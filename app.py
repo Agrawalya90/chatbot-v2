b3c49c8480268e6ec23e6818abd8f2fcdca8d4d2
Agrawalya90 <agrawalya90@gmail.com>
2025-11-06 14:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -9,11 +8,9 @@ import numpy as np
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
-load_dotenv() 
-
 # Load Supabase credentials
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = "https://wmroscjdhemybxznwmfg.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indtcm9zY2pkaGVteWJ4em53bWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MzY0OTgsImV4cCI6MjA3MTExMjQ5OH0.RaK3ROSAzD9-eeDVW28fkZGPS9i53bycJM9T88GlPRU"
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     st.error("Missing Supabase credentials. Add them to your .env file.")
